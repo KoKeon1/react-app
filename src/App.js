e28b2a31854bc1bcc5b6aa1d8c28893963fe2fcc
KoKeon1 <rjaakel1@naver.com>
2022-07-31 15:46:16 +0900

    리액트 컴포넌트 공부

--- src/App.js
+++ src/App.js
@@ -7,22 +7,32 @@ function Header(){
          </header>
 }
 
+function Nav(){
+  return <nav>
+          <ol>
+            <li><a href='/read/1'>html2</a></li>
+            <li><a href='/read/2'>css2</a></li>
+            <li><a href='/read/3'>js2</a></li>
+          </ol>
+        </nav>
+}
+
+function Article(){
+  return <article>
+          <h2 color='red'>welcome222</h2>
+        </article>
+}
+
 function App() {
   return (
     <div>
       <Header></Header>
       <Header></Header>
       <Header></Header>
-      <nav>
-        <ol>
-          <li><a href='/read/1'>html</a></li>
-          <li><a href='/read/2'>css</a></li>
-          <li><a href='/read/3'>js</a></li>
-        </ol>
-      </nav>
-      <article>
-        <h2 color='red'>welcome</h2>
-      </article>
+      
+      <Nav></Nav>
+      
+      <Article></Article>
     </div>
   );
 }
